d44bab3ec6db1bb4dc475a903d8f601a65624f1c
Pavel Kovalenko <greenman.mail@gmail.com>
2013-07-16 23:09:52 +0800

    Works fast in SOPs, not working at all in Mantra. Hardcoded parameters

--- vexVDB.cpp
+++ vexVDB.cpp
@@ -26,44 +26,64 @@
  * This is a sample VEX operator DSO
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
-#include <math.h>
-#include <UT/UT_DSOVersion.h>
-#include <UT/UT_Thread.h>
-#include <VEX/VEX_VexOp.h>
-#include <UT/UT_WorkBuffer.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <time.h>
+// #include <math.h>
+// #include <UT/UT_DSOVersion.h>
+// #include <UT/UT_Thread.h>
+// #include <VEX/VEX_VexOp.h>
+// #include <UT/UT_WorkBuffer.h>
 
-#include <cstring>
-#include <UT/UT_DirUtil.h>
-#include <UT/UT_String.h>
-#include <UT/UT_Vector.h>
+// #include <cstring>
+// #include <UT/UT_DirUtil.h>
+// #include <UT/UT_String.h>
+// #include <UT/UT_Vector.h>
 
-#include <openvdb/openvdb.h>
+// #include <openvdb/openvdb.h>
+#include <vexVDB.h>
 
 using namespace std;
 
 
+//static vdbGrid oneGrid();
+bool                    vdbGrid::fileIsOpened_ = false;
+openvdb::io::File       vdbGrid::vdbFile_("/home/green/Downloads/smoke.vdb");
+openvdb::GridBase::Ptr  vdbGrid::baseGrid_;// = vdbGrid::vdbFile_.readGrid("density");
+//UT_Vector3*             RainData::pointInitialPositions_;
+void vdbGrid::initialize(const char* fileName)
+{
+    openvdb::GridBase::Ptr baseGrid_;
+    openvdb::initialize();
+    string fp(fileName);
+    //vdbGrid::vdbFile_( fp );
 
+    vdbGrid::vdbFile_.open();
+    vdbGrid::baseGrid_ = vdbFile_.readGrid("density");
 
+    vdbGrid::setState(true);
+}
 
-class vdbGrid {
-public:
-     vdbGrid() { }
-    ~vdbGrid() { }
-
-    //float       evaluate(float v)       { return 0; }
-
-    //int         myRefCount;
-};
-
-static vdbGrid      *oneGrid = NULL;
+void vdbGrid::cleanup()
+{
+    vdbGrid::vdbFile_.close();
+    vdbGrid::setState(false);
+}
 
 static void *
 pre_readVDB()
 {
+
+    bool isfileNameValid = true;
+    const char* fileName("/home/green/Downloads/smoke.vdb");
+
+    if( (vdbGrid::isInitialized() == 0) && (isfileNameValid == true) )
+    {
+        vdbGrid::initialize(fileName);
+    }
+    //vdbGrid oneGrid;
+
 
     // if (!theGammaTable)
     //     theGammaTable = new gamma_Table();
@@ -76,6 +96,7 @@ pre_readVDB()
 static void
 post_readVDB(void *data)
 {
+    vdbGrid::cleanup();
     // gamma_Table *table = (gamma_Table *)data;
 
     // UT_ASSERT(table == theGammaTable);
@@ -98,58 +119,62 @@ post_readVDB(void *data)
 // }
 
 
-
-
-
-
-
 void readVDB(int argc, void *argv[], void *)
 {
 
     float* result = (float *)argv[0];
-    const char* filepath( (const char *)argv[1] );
+    const char* fileName( (const char *)argv[1] );
     const UT_Vector3* worldCoord = (const UT_Vector3 *)argv[2];
-    const bool isFilepathValid = UTisValidRegularFile( filepath );
-    //printf("0: %i\n", isFilepathValid);
-    openvdb::initialize();
-    //printf("1: %i\n", isFilepathValid);
+
+    const bool isfileNameValid = UTisValidRegularFile( fileName );
+
+
+    //printf("Is Initialized 1: %i\n", vdbGrid::isInitialized());
+
+
+
+    //printf("Is Initialized 2:%i\n", vdbGrid::isInitialized());
+    //printf("1: %i\n", isfileNameValid);
 
     //float value = 0.001;
     //float* cellValue;
-    
     //*cellValue = value;
     float pAccesedValue = 0;
-    if( isFilepathValid == 1 )
-    {   
-        //printf("foo\n");
-        //printf("1: %s\n", filepath);
-        openvdb::io::File file( filepath );
+    if(vdbGrid::isInitialized())
+    {
+        // if( isfileNameValid == 1 )
+        // {   
+        //     //printf("foo\n");
+        //     //printf("1: %s\n", fileName);
+        //     openvdb::io::File file( fileName );
 
-        file.open();
-        openvdb::GridBase::Ptr baseGrid;
-        baseGrid = file.readGrid("density");
-        //printf("%s\n", baseGrid->type());
-        //cout << baseGrid->activeVoxelCount () << endl;
+        //     file.open();
+        //     openvdb::GridBase::Ptr baseGrid_;
+        //     baseGrid_ = file.readGrid("density");
+        //     //printf("%s\n", baseGrid_->type());
+        //     //cout << baseGrid_->activeVoxelCount () << endl;
 
         const openvdb::Vec3d samplePosition(worldCoord->x(),worldCoord->y(),worldCoord->z());
-        const openvdb::Vec3i indexPosition = baseGrid->worldToIndex(samplePosition);
-        //printf("pos: %4.2f %4.2f %4.2f\n", worldCoord->x(), worldCoord->y(), worldCoord->z());
-        //printf("index: %4.2i %4.2i %4.2i\n\n", indexPosition[0], indexPosition[1], indexPosition[2]);
+        const openvdb::Vec3i indexPosition = vdbGrid::baseGrid_->worldToIndex(samplePosition);
+        //     //printf("pos: %4.2f %4.2f %4.2f\n", worldCoord->x(), worldCoord->y(), worldCoord->z());
+        //     //printf("index: %4.2i %4.2i %4.2i\n\n", indexPosition[0], indexPosition[1], indexPosition[2]);
 
-        openvdb::FloatGrid::Ptr grid = openvdb::gridPtrCast<openvdb::FloatGrid>(baseGrid);
+        openvdb::FloatGrid::Ptr grid = openvdb::gridPtrCast<openvdb::FloatGrid>(vdbGrid::baseGrid_);
         openvdb::FloatGrid::Accessor accessor = grid->getAccessor();
         const openvdb::Coord xyz(indexPosition);
         pAccesedValue = accessor.getValue(xyz);
-        //*val = pAccesedValue;
+        //     //*val = pAccesedValue;
 
-        file.close();
-    }
-    else
-    {
-        printf("Error reading file %s\n", filepath);
+        //     file.close();
+        // }
+        // else
+        // {
+        //     printf("Error reading file %s\n", fileName);
+        // }
+
+        *result = pAccesedValue;
     }
 
-    *result = pAccesedValue;
 }
 
 void newVEXOp(void *)
@@ -167,8 +192,8 @@ void newVEXOp(void *)
     new VEX_VexOp ( "readVDB@&FSV",
                     readVDB,
                     VEX_ALL_CONTEXT,
-                    NULL,
-                    NULL);
+                    pre_readVDB,
+                    post_readVDB);
 
 }
 
