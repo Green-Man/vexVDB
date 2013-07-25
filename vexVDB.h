5dc360180865884c134fb16778a31ddce6bf2f88
Pavel Kovalenko <greenman.mail@gmail.com>
2013-07-25 22:09:45 +0800

    don't forget header

--- vexVDB.h
+++ vexVDB.h
@@ -1,15 +1,15 @@
 #ifndef VEXVDB
 #define VEXVDB
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
-#include <math.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <unistd.h>
+//#include <time.h>
+//#include <math.h>
 #include <UT/UT_DSOVersion.h>
-#include <UT/UT_Thread.h>
+//#include <UT/UT_Thread.h>
 #include <VEX/VEX_VexOp.h>
-#include <UT/UT_WorkBuffer.h>
+//#include <UT/UT_WorkBuffer.h>
 
 #include <cstring>
 #include <UT/UT_DirUtil.h>
@@ -18,6 +18,10 @@
 
 #include <openvdb/openvdb.h>
 
+#include <openvdb/tools/Filter.h>
+#include <openvdb/tools/Interpolation.h>
+#include <openvdb/tree/ValueAccessor.h>
+
 using namespace std;
 
 class vdbGrid {
@@ -25,25 +29,14 @@ public:
     vdbGrid();
     ~vdbGrid();
 
-    //float       evaluate(float v)       { return 0; }
-
-    //int         myRefCount;
-    bool isInitialized()
-    {
-        return fileIsOpened_;
-    }
-    void setState(bool state)
-    {
-        fileIsOpened_ = state;
-    }
-    void initialize(const char* fileName);
-    void cleanup();
-
-
-    bool                        fileIsOpened_;
-    openvdb::GridBase::Ptr      baseGrid_;
-    openvdb::io::File*           vdbFile_;
+    bool                                        isFileOpened_;
+    openvdb::GridBase::Ptr                      baseGrid_;
+    openvdb::io::File*                          vdbFile_;
+    string                                      fileName_;
+    openvdb::FloatGrid::Ptr                     floatGrid_; 
+    openvdb::tools::Filter<openvdb::FloatGrid>*  filteredGrid_;
 private:
+
 };
 
 
